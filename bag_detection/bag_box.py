ec1d09234d105a46c9023c045ea4138fb4c12918
ntheis2018 <44625786+ntheis2018@users.noreply.github.com>
2018-12-05 19:27:05 -0500

    Added 'Color Splash' Functionality

--- bag_detection/bag_box.py
+++ bag_detection/bag_box.py
@@ -40,16 +40,56 @@ def detect_and_box(model, image_path):
     # Save output
     file_name = "box_{:%Y%m%dT%H%M%S}.png".format(datetime.datetime.now())
     full_file_path = os.path.join(DIR,file_name)
-    print('ffp',full_file_path)
-    cv2.imwrite(full_file_path, img)
+#    print('ffp',full_file_path)
+#    cv2.imwrite(full_file_path, img)
 
     mod_path = DIR + '\\' + file_name
     print('mod_path:')
     print(mod_path)
 
-    return r['rois'], mod_path
+    return r['rois'], mod_path, r['masks']
 
+# From Mask R-CNN Balloon Sample #############################
+def color_splash(image, mask):
+    """Apply color splash effect.
+    image: RGB image [height, width, 3]
+    mask: instance segmentation mask [height, width, instance count]
+
+    Returns result image.
+    """
+    # Make a grayscale copy of the image. The grayscale copy still
+    # has 3 RGB channels, though.
+    gray = skimage.color.gray2rgb(skimage.color.rgb2gray(image)) * 255
+    # Copy color pixels from the original color image where mask is set
+    if mask.shape[-1] > 0:
+        # We're treating all instances as one, so collapse the mask into one layer
+        mask = (np.sum(mask, -1, keepdims=True) >= 1)
+        splash = np.where(mask, image, gray).astype(np.uint8)
+    else:
+        splash = gray.astype(np.uint8)
+    return splash
+
+
+def detect_and_color_splash(masks, image_path=None):
+
+    # Run model detection and generate the color splash effect
+    print("Running on {}".format(image_path))
+    # Read image
+    image = skimage.io.imread(image_path)
+    # Detect objects
+#    r = model.detect([image], verbose=1)[0]
+    # Color splash
+    splash = color_splash(image, masks)
+    # Save output
+    file_name = "splash_{:%Y%m%dT%H%M%S}.png".format(datetime.datetime.now())
+    skimage.io.imsave(file_name, splash)
+
+    splash_path = DIR + '\\' + file_name
+
+    return splash_path
+# #############################################################
     
+
 def run(image_path):
 
     # Configurations
@@ -72,6 +112,8 @@ def run(image_path):
     model.load_weights(weights_path, by_name=True)
 
     # Evaluate the image for bags
-    points, paths = detect_and_box(model, image_path)
+    points, paths, masks = detect_and_box(model, image_path)
 
-    return points, paths
+    paths_color = detect_and_color_splash(masks, image_path)
+
+    return points, paths_color
